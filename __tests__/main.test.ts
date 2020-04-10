064367dc6948f5d0d893dd2683996d6756aa70c1
Vitalii Hrechko <vitalii.hrechko@ardas.biz>
2020-04-10 18:53:04 +0300

    implement skeleton action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,14 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as core from '@actions/core'
+import {run} from '../src/main'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+jest.mock('@actions/core')
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+describe('When running the action', () => {
+   const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
+
+   test('it should set the release-url output parameter', async () => {
+     await run()
+     expect(fakeSetOutput).toHaveBeenLastCalledWith('release-url', expect.anything())
+   })
 })
